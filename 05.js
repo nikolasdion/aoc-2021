677f6530a90222bfebb3df23cde49d377697d2ae
Dion Susanto <nikolasdion@gmail.com>
2022-01-08 20:34:36 +0000

    Add solution to day 5 part 2

--- 05.js
+++ 05.js
@@ -10,12 +10,17 @@ const lines = input.map((rawLine) => {
   };
 });
 
-const emptyRow = new Array(1000).fill(0);
-const emptyBoard = new Array(1000).fill(null).map(() => [...emptyRow]);
+function emptyBoard() {
+  const emptyRow = new Array(1000).fill(0);
+  return new Array(1000).fill(null).map(() => [...emptyRow]);
+}
 
-function yLine(line, board) {
-  if (line.start[0] !== line.end[0]) return;
+function isYLine(line) {
+  return line.start[0] === line.end[0];
+}
 
+// Lines from (p,q) => (p,r)
+function drawYLine(line, board) {
   const x = line.start[0];
   const yStart = Math.min(line.start[1], line.end[1]);
   const yEnd = Math.max(line.start[1], line.end[1]);
@@ -25,7 +30,12 @@ function yLine(line, board) {
   }
 }
 
-function xLine(line, board) {
+function isXLine(line) {
+  return line.start[1] === line.end[1];
+}
+
+// Lines from (p,q) => (r,q)
+function drawXLine(line, board) {
   if (line.start[1] !== line.end[1]) return;
 
   const y = line.start[1];
@@ -37,22 +47,75 @@ function xLine(line, board) {
   }
 }
 
+function isDiagonalUpLine(line) {
+  return line.start[0] - line.end[0] === line.start[1] - line.end[1];
+}
+
+// Lines from (p,q) => (p+r,q+r)
+function drawDiagonalUpLine(line, board) {
+  const xStart = Math.min(line.start[0], line.end[0]);
+  const yStart = Math.min(line.start[1], line.end[1]);
+
+  const xEnd = Math.max(line.start[0], line.end[0]);
+
+  const range = xEnd - xStart;
+
+  for (let offset = 0; offset < range + 1; offset++) {
+    board[xStart + offset][yStart + offset] += 1;
+  }
+}
+
+function isDiagonalDownLine(line) {
+  return line.start[0] - line.end[1] === line.end[0] - line.start[1];
+}
+
+// Lines from (p,q) => (p+r,q-r)
+function drawDiagonalDownLine(line, board) {
+  const xStart = Math.min(line.start[0], line.end[0]);
+  const yStart = Math.max(line.start[1], line.end[1]);
+
+  const xEnd = Math.max(line.start[0], line.end[0]);
+
+  const range = xEnd - xStart;
+
+  for (let offset = 0; offset < range + 1; offset++) {
+    board[xStart + offset][yStart - offset] += 1;
+  }
+}
+
 function drawXOrYLine(line, board) {
-  xLine(line, board);
-  yLine(line, board);
+  if (isXLine(line)) drawXLine(line, board);
+  else if (isYLine(line)) drawYLine(line, board);
+}
+
+function drawAnyLine(line, board) {
+  if (isXLine(line)) drawXLine(line, board);
+  else if (isYLine(line)) drawYLine(line, board);
+  else if (isDiagonalUpLine(line)) drawDiagonalUpLine(line, board);
+  else if (isDiagonalDownLine(line)) drawDiagonalDownLine(line, board);
 }
 
 function countIntersections(board) {
-  return board.flat().reduce((count, value) => count + (value >= 2 ? 1 : 0));
+  let count = 0;
+
+  board.flat().forEach((number) => {
+    if (number >= 2) count += 1;
+  });
+
+  return count;
 }
 
 function part1() {
-  const board = [...emptyBoard];
+  const board = emptyBoard();
   lines.forEach((line) => drawXOrYLine(line, board));
   return countIntersections(board);
 }
 
-function part2() {}
+function part2() {
+  const board = emptyBoard();
+  lines.forEach((line) => drawAnyLine(line, board));
+  return countIntersections(board);
+}
 
 console.log(`The answer to part 1 is: ${part1()}`);
 console.log(`The answer to part 2 is: ${part2()}`);
